17d989f15dfdafa8fbc4cb0c39207d0e6a33cd24
Jayme Anunciação <jaymeanunciacao.hrzon@gmail.com>
2025-03-09 12:26:14 -0300

    feature(src/infra/repositories/memory/user-repository): Implementa repositorio de usuario em memoria.

---
+++ src/infra/repositories/memory/user-repository.ts
@@ -0,0 +1,31 @@
+import Contract from "../../../application/contracts/repositories/user-repository";
+import User from "../../../domain/entities/user";
+import UserNotFoundException from "../../exceptions/user-not-found-exceptions";
+
+export default class UserRepository implements Contract {
+  private database: User[];
+
+  public constructor() {
+    this.database = [];
+  }
+
+  public find(cpf: string): Promise<User> {
+    return new Promise((resolve, reject) => {
+      const user = this.database.find((user) => user.equals(cpf));
+
+      if (!user) {
+        reject(new UserNotFoundException());
+      } else {
+        resolve(user);
+      }
+    });
+  }
+
+  public create(user: User): Promise<void> {
+    return new Promise((resolve) => {
+      this.database.push(user);
+
+      return resolve();
+    })
+  }
+}
