5ce0971ef721db4862969ebcdcaafae57a0f5527
Jayme Anunciação <jaymeanunciacao.hrzon@gmail.com>
2025-03-09 19:20:36 -0300

    feature(application/services): Implementa metodo para a validacao do token.

--- src/application/services/token-service.ts
+++ src/application/services/token-service.ts
@@ -14,4 +14,22 @@ export default class TokenService implements Contract {
       resolve(accessToken);
     });
   }
+
+  public validateToken(accessToken: string): Promise<{ cpf: string; }> {
+    return new Promise((resolve, reject) => {
+      const splitedToken = accessToken.split(/\s+/);
+
+      if (splitedToken[0] !== 'Bearer') {
+        reject(new Error("Token invalido"))
+      }
+
+      const payload = jwt.verify(splitedToken[1], JWT_SECRET);
+
+      if (payload) {
+        resolve({ cpf: payload as string })
+      } else {
+        reject(new Error("Token invalido"))
+      }
+    });
+  }
 }
