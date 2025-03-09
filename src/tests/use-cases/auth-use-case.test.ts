916f2e7b386ba00707caeebde45263dd53cd82e4
Jayme Anunciação <jaymeanunciacao.hrzon@gmail.com>
2025-03-09 16:03:06 -0300

    test(src/tests/use-cases): Adiciona teste para validacao de envio do cpf.

--- src/tests/use-cases/auth-use-case.test.ts
+++ src/tests/use-cases/auth-use-case.test.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from '@jest/globals';
 import AuthUseCase from '../../application/use-cases/auth-use-case';
 import UserMemoryRepository from '../../infra/repositories/memory/user-repository';
 import TokenService from '../../application/services/token-service';
+import CpfNotProvidedException from '../../application/exceptions/cpf-not-provided-exception';
 
 describe('Teste do caso de uso de autenticacao', function () {
   it('Deve autenticar o usuario com cpf ja informado', async () => {
@@ -16,5 +17,16 @@ describe('Teste do caso de uso de autenticacao', function () {
 
     expect(output.code).toBe('AUTHENTICATED');
     expect(output.accessToken).toBeDefined();
+  });
+
+  it('Nao deve autenticar o usuario caso o cpf nao seja informado', async () => {
+    const cpf = '';
+
+    const useCase = new AuthUseCase(
+      new UserMemoryRepository(),
+      new TokenService(),
+    );
+
+    expect(async () => await useCase.execute(cpf)).rejects.toThrow(CpfNotProvidedException);
   });
 });
