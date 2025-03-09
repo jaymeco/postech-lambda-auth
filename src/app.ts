4fda993c532d580f84c064c4bed3df57884741f8
Jayme Anunciação <jaymeanunciacao.hrzon@gmail.com>
2025-03-09 16:16:42 -0300

    feature: Adiciona handler basico para a autorizacao.

--- src/app.ts
+++ src/app.ts
@@ -37,3 +37,12 @@ export const authHandler = async (event: any): Promise<any> => {
         };
     }
 };
+
+export const authorizationHandler = async (event: any): Promise<any> => {
+    return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Basic test!',
+            }),
+        };
+}
