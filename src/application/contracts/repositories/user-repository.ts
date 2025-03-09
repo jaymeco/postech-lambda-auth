a11ffd39ef932a4cd1c650036db5d924a07dc301
Jayme Anunciação <jaymeanunciacao.hrzon@gmail.com>
2025-03-09 19:00:11 -0300

    feature(application/contracts/repositories): Adiciona assinatura para o metodo de existencia.

--- src/application/contracts/repositories/user-repository.ts
+++ src/application/contracts/repositories/user-repository.ts
@@ -3,4 +3,5 @@ import User from "../../../domain/entities/user";
 export default interface UserRepository {
   find(cpf: string): Promise<User>;
   create(user: User): Promise<void>;
+  exists(cpf: string): Promise<boolean>
 }
