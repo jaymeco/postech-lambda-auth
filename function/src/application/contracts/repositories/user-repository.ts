af519fd11b84a7257738527169e4c356d28cadc3
jaymeco <jayme.anunciacao@gmail.com>
2025-03-15 12:41:15 -0300

    refactor(application/contracts/repositories): Adiciona symbol para as interfaces de contrato.

--- function/src/application/contracts/repositories/user-repository.ts
+++ function/src/application/contracts/repositories/user-repository.ts
@@ -1,5 +1,7 @@
 import User from "../../../domain/entities/user";
 
+export const UserRepositorySymbol = Symbol('UserRepository');
+
 export default interface UserRepository {
   find(cpf: string): Promise<User>;
   create(user: User): Promise<void>;
--- function/src/application/contracts/services/token-service.ts
+++ function/src/application/contracts/services/token-service.ts
@@ -1,3 +1,5 @@
+export const TokenServiceSymbol = Symbol('TokenService');
+
 export default interface TokenService {
   generateToken(cpf: string): Promise<string>;
   validateToken(token: string): Promise<{cpf: string}>
