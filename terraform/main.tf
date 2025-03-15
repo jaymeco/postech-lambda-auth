f570c1d47be881e4da277c494611478290f8acea
jaymeco <jayme.anunciacao@gmail.com>
2025-03-15 17:04:07 -0300

    cd(terraform): Adiciona configuracoes para deploy de artefatos com o node_modules.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,28 +3,44 @@ variable "LabRole" {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region                   = "us-east-1"
   shared_credentials_files = ["/home/jaymeco/.aws/credentials"]
 }
 
+resource "null_resource" "build_modules" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/build.sh"
+  }
+}
+
 data "archive_file" "zip_application" {
-  type = "zip"
-  source_dir = "${path.module}/../function/dist"
+  depends_on  = [null_resource.build_modules]
+  type        = "zip"
+  source_dir  = "${path.module}/../function/dist"
   output_path = "${path.module}/../function/dist.zip"
 }
 
-resource "aws_lambda_function" "terraform-authentication-lambda" {
-  role = var.LabRole
-  filename = "${path.module}/../function/dist.zip"
-  function_name = "authentication-lambda"
-  runtime = "nodejs22.x"
-  handler = "app.authHandler"
-}
+resource "aws_lambda_function" "terraform-functions-lambda" {
+  depends_on = [data.archive_file.zip_application]
+  for_each   = local.lambdas
 
-resource "aws_lambda_function" "terraform-authorization-lambda" {
-  role = var.LabRole
+  role     = var.LabRole
   filename = "${path.module}/../function/dist.zip"
-  function_name = "authorization-lambda"
-  runtime = "nodejs22.x"
-  handler = "app.authorizationHandler"
+
+  function_name = "${each.key}-lambda"
+  handler       = "app.${each.key}Handler"
+  description   = each.value["description"]
+  runtime       = "nodejs22.x"
+
+  environment {
+    variables = {
+      JWT_SECRET = "sample"
+
+      DYNAMO_TABLE_NAME = "postech"
+    }
+  }
 }
